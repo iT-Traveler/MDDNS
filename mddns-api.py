e2d0065aef3e7093d35a4bd4d54a4806e15f6ae2
iT-Traveler <95235625+iT-Traveler@users.noreply.github.com>
2024-04-05 14:16:00 +0200

    Update mddns-api.py
    Fixed having to enter entire url instead of only sub domain

--- mddns-api.py
+++ mddns-api.py
@@ -8,7 +8,8 @@ from dnszone import DnsZone
 from mongodb_functions import *
 u = UserFunctions()
 
-dns_zone = DnsZone("mddns.local", "172.201.251.154")
+domain_name = "mddns.local"
+dns_zone = DnsZone(domain_name, "172.201.251.154")
 
 app = Flask(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -30,42 +31,47 @@ class NewSubDomain(Resource):
         args = parser.parse_args()
         if DEBUG:
             app.logger.warning(f"DEBUG: NewSubDomain.post() <- {args }")
-        sub_domain = args['subdomain']
+        subdomain = args['subdomain']
+        fqdn = f"{subdomain}.{domain_name}"
         ipv4 = args['ip']
-        dns_zone.add_address(sub_domain, ipv4)
+        dns_zone.add_address(fqdn, ipv4)
         return {'status': 'ok'}
 
 class EditSubDomain(Resource):
     # Endpoint voor het verwijderen van een subdomein.
-    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test1 -X DELETE
-    def delete(self, fqdn):
+    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test -X DELETE
+    def delete(self, sub_domain):
         if DEBUG:
-            app.logger.warning(f"DEBUG: EditSubDomain.delete(\"{fqdn}\")")
+            app.logger.warning(f"DEBUG: EditSubDomain.delete(\"{sub_domain}\")")
+        fqdn = f"{sub_domain}.{domain_name}"
         dns_zone.clear_address(fqdn)
         return {'status': 'ok'}
 
     # Endpoint voor het bijwerken van een subdomein.
-    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test1 -H "Content-type: application/json" -d "{ \"ip\" : \"1.1.1.1\" }" -X PUT
-    def put(self, fqdn):
+    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test -H "Content-type: application/json" -d "{ \"ip\" : \"1.1.1.1\" }" -X PUT
+    def put(self, sub_domain):
         args = parser.parse_args()
         if DEBUG:
-            app.logger.warning(f"DEBUG: EditSubDomain.put(\"{fqdn}\") <- {args }")
+            app.logger.warning(f"DEBUG: EditSubDomain.put(\"{sub_domain}\") <- {args }")
+        fqdn = f"{sub_domain}.{domain_name}"
         ipv4 = args['ip']
         update_result = dns_zone.update_address(fqdn, ipv4)
         return {'status': 'ok'}
 
     # Endpoint voor het ophalen van details van een subdomein.
-    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test1
-    def get(self, fqdn):
+    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test
+    def get(self, sub_domain):
         if DEBUG:
-            app.logger.warning(f"DEBUG: EditSubDomain.get(\"{fqdn}\")")
+            app.logger.warning(f"DEBUG: EditSubDomain.get(\"{sub_domain}\")")
+        # Check the status of the A record for the sub_domain
+        fqdn = f"{sub_domain}.{domain_name}"
         domain_details = dns_zone.check_address(fqdn)
-        return {domain_details}
+        return domain_details
 
 # Definieer endpoints hieronder.
-# Als een deel van de URL een variabele is, gebruik dan haakjes, bijvoorbeeld <fqdn>
+# Als een deel van de URL een variabele is, gebruik dan haakjes, bijvoorbeeld <sub_domain>
 api.add_resource(NewSubDomain, '/subdomain/new')
-api.add_resource(EditSubDomain, '/subdomain/name/<fqdn>')
+api.add_resource(EditSubDomain, '/subdomain/name/<sub_domain>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
