d8dad96d3d51f7642b8627ec005f2ab89e1132f3
iT-Traveler <95235625+iT-Traveler@users.noreply.github.com>
2024-04-07 15:10:31 +0200

    Update mddns-api.py
    Added request Autorisation validation by using api-keys. API keys are stored in api_keys.json

--- mddns-api.py
+++ mddns-api.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 import logging
 import sys
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
+import json
+from flask import Flask, request, abort
+from flask_restful import reqparse, Api, Resource
 from dnszone import DnsZone
-
-from mongodb_functions import *
-u = UserFunctions()
 
 domain_name = "mddns.local"
 dns_zone = DnsZone(domain_name, "172.201.251.154")
@@ -24,9 +22,27 @@ parser.add_argument('ip')
 parser.add_argument('code')
 parser.add_argument('value')
 
+
+# Haal de api keys op uit het bestand
+with open('api_keys.json', 'r') as f:
+    VALID_API_KEYS = json.load(f)
+
+
+# Valideer de opgegeven API keys
+def require_api_key(func):
+    def wrapper(*args, **kwargs):
+        api_key = request.headers.get('X-API-Key')
+        if api_key in VALID_API_KEYS:
+            return func(*args, **kwargs)
+        else:
+            abort(401, 'Unauthorized: Invalid API Key')
+    return wrapper
+
+
 class NewSubDomain(Resource):
-    # Endpoint voor het toevoegen van een nieuwe subdomein.
-    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/new -H "Content-type: application/json" -d "{ \"subdomain\" : \"miro\", \"ip\" : \"1.1.1.1\" }" -X POST
+    # Endpoint voor het toevoegen van een nieuw subdomein.
+    # Voorbeeld curl -X POST https://<ip_of_server>:<port>/subdomain/new -H "Content-type: application/json" -H "X-API-Key: your_api_key" -d "{ \"subdomain\" : \"miro\", \"ip\" : \"1.1.1.1\" }"
+    @require_api_key
     def post(self):
         args = parser.parse_args()
         if DEBUG:
@@ -39,7 +55,8 @@ class NewSubDomain(Resource):
 
 class EditSubDomain(Resource):
     # Endpoint voor het verwijderen van een subdomein.
-    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test -X DELETE
+    # Voorbeeld curl -X DELETE https://<ip_of_server>:<port>/subdomain/name/test -H "X-API-Key: your_api_key"
+    @require_api_key
     def delete(self, sub_domain):
         if DEBUG:
             app.logger.warning(f"DEBUG: EditSubDomain.delete(\"{sub_domain}\")")
@@ -48,7 +65,8 @@ class EditSubDomain(Resource):
         return {'status': 'ok'}
 
     # Endpoint voor het bijwerken van een subdomein.
-    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test -H "Content-type: application/json" -d "{ \"ip\" : \"1.1.1.1\" }" -X PUT
+    # Voorbeeld curl -X PUT https://<ip_of_server>:<port>/subdomain/name/test -H "Content-type: application/json" -H "X-API-Key: your_api_key" -d "{ \"ip\" : \"1.1.1.1\" }"
+    @require_api_key
     def put(self, sub_domain):
         args = parser.parse_args()
         if DEBUG:
@@ -59,7 +77,8 @@ class EditSubDomain(Resource):
         return {'status': 'ok'}
 
     # Endpoint voor het ophalen van details van een subdomein.
-    # Voorbeeld curl: http://192.168.1.158:5001/subdomain/name/test
+    # Voorbeeld curl https://<ip_of_server>:<port>/subdomain/name/test -H "X-API-Key: your_api_key"
+    @require_api_key
     def get(self, sub_domain):
         if DEBUG:
             app.logger.warning(f"DEBUG: EditSubDomain.get(\"{sub_domain}\")")
@@ -74,4 +93,4 @@ api.add_resource(NewSubDomain, '/subdomain/new')
 api.add_resource(EditSubDomain, '/subdomain/name/<sub_domain>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=80)
