1c306d158a926393e462bd12369296c0b7387e2b
iT-Traveler <95235625+iT-Traveler@users.noreply.github.com>
2024-04-07 21:59:35 +0200

    Update mddns-api.py

--- API/mddns-api.py
+++ API/mddns-api.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
 import logging
 import sys
-import json
 from flask import Flask, request, abort
 from flask_restful import reqparse, Api, Resource
 from dnszone import DnsZone
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+import certifi
+from datetime import datetime
+
+ca = certifi.where()
+
+# MongoDB Atlas cluster
+load_dotenv()
+connection_string: str = os.environ.get("CONNECTION_STRING")
+mongo_client: MongoClient = MongoClient(connection_string, tlsCAFile=ca)
+
+# MongoDB database en collection
+database: Database = mongo_client.get_database("mddns-database")
+userCollection: Collection = database.get_collection("users")
+recordCollection: Collection = database.get_collection("records")
 
 domain_name = "mddns.local"
 dns_zone = DnsZone(domain_name, "172.201.251.154")
@@ -14,7 +33,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 api = Api(app)
 
-DEBUG = True   # zet op False wanneer klaar met testen en troubleshooten
+DEBUG = True  # zet op False wanneer klaar met testen en troubleshooten
 
 parser = reqparse.RequestParser()
 parser.add_argument('subdomain')
@@ -23,19 +42,16 @@ parser.add_argument('code')
 parser.add_argument('value')
 
 
-# Haal de api keys op uit het bestand
-with open('api_keys.json', 'r') as f:
-    VALID_API_KEYS = json.load(f)
-
-
 # Valideer de opgegeven API keys
 def require_api_key(func):
     def wrapper(*args, **kwargs):
         api_key = request.headers.get('X-API-Key')
-        if api_key in VALID_API_KEYS:
+        valid_api_keys = userCollection.distinct("api-key")
+        if api_key in valid_api_keys:
             return func(*args, **kwargs)
         else:
             abort(401, 'Unauthorized: Invalid API Key')
+
     return wrapper
 
 
@@ -45,35 +61,57 @@ class NewSubDomain(Resource):
     @require_api_key
     def post(self):
         args = parser.parse_args()
+        api_key = request.headers.get('X-API-Key')
         if DEBUG:
-            app.logger.warning(f"DEBUG: NewSubDomain.post() <- {args }")
+            app.logger.warning(f"DEBUG: NewSubDomain.post() <- {args}")
+        # Haal alle variabelen op
         subdomain = args['subdomain']
         fqdn = f"{subdomain}.{domain_name}"
         ipv4 = args['ip']
+        timestamp = datetime.now()
+        # CHECK of het record al bestaat
+        check_exists = recordCollection.find_one({"subdomain": subdomain})
+        if check_exists:
+            abort(409, f"Record with subdomain {subdomain} already exists.")
+        # Voeg het nieuwe record toe door middel van de dns_zone module
         dns_zone.add_address(fqdn, ipv4)
+        # Schrijf het nieuwe record weg in de database
+        record = {"api-key": api_key, "subdomain": subdomain, "ipv4": ipv4, "timestamp": timestamp}
+        recordCollection.insert_one(record)
         return {'status': 'ok'}
+
 
 class EditSubDomain(Resource):
     # Endpoint voor het verwijderen van een subdomein.
     # Voorbeeld curl -X DELETE https://<ip_of_server>:<port>/subdomain/name/test -H "X-API-Key: your_api_key"
     @require_api_key
     def delete(self, sub_domain):
+        api_key = request.headers.get('X-API-Key')
         if DEBUG:
             app.logger.warning(f"DEBUG: EditSubDomain.delete(\"{sub_domain}\")")
         fqdn = f"{sub_domain}.{domain_name}"
+        # Verwijder het record door middel van de dns_zone module
         dns_zone.clear_address(fqdn)
+        # Verwijder het record uit de database
+        recordCollection.delete_one({"subdomain": sub_domain})
         return {'status': 'ok'}
 
     # Endpoint voor het bijwerken van een subdomein.
     # Voorbeeld curl -X PUT https://<ip_of_server>:<port>/subdomain/name/test -H "Content-type: application/json" -H "X-API-Key: your_api_key" -d "{ \"ip\" : \"1.1.1.1\" }"
     @require_api_key
     def put(self, sub_domain):
+        api_key = request.headers.get('X-API-Key')
         args = parser.parse_args()
         if DEBUG:
-            app.logger.warning(f"DEBUG: EditSubDomain.put(\"{sub_domain}\") <- {args }")
+            app.logger.warning(f"DEBUG: EditSubDomain.put(\"{sub_domain}\") <- {args}")
         fqdn = f"{sub_domain}.{domain_name}"
         ipv4 = args['ip']
-        update_result = dns_zone.update_address(fqdn, ipv4)
+        timestamp = datetime.now()
+        # Update het record door middel van de dns_zone module
+        dns_zone.update_address(fqdn, ipv4)
+        # Update het record in de database
+        recordCollection.update_one({"subdomain": sub_domain},
+                                    {"api-key": api_key, "subdomain": sub_domain, "ipv4": ipv4, "timestamp": timestamp})
         return {'status': 'ok'}
 
     # Endpoint voor het ophalen van details van een subdomein.
@@ -87,10 +125,11 @@ class EditSubDomain(Resource):
         domain_details = dns_zone.check_address(fqdn)
         return domain_details
 
+
 # Definieer endpoints hieronder.
 # Als een deel van de URL een variabele is, gebruik dan haakjes, bijvoorbeeld <sub_domain>
 api.add_resource(NewSubDomain, '/subdomain/new')
 api.add_resource(EditSubDomain, '/subdomain/name/<sub_domain>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5001)
